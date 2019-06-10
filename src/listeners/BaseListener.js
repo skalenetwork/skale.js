8eaad9fb612966ee862536b00b75db73b719defd
elvisgiv <alexeymaratovich@gmail.com>
2019-06-10 10:47:29 +0300

    SKALE-1140 Update eslint dependency

--- src/listeners/BaseListener.js
+++ src/listeners/BaseListener.js
@@ -49,7 +49,7 @@ class BaseListener {
 
     turnOff() {
         let self = this;
-        this.subscription.unsubscribe(function (error, success) {
+        this.subscription.unsubscribe(function (success) {
             if (success) {
                 self.active = false;
             }
